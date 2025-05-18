4953346d5bae637e998ac6d0e4720ddeef2ec50e
Vight <vinhtan1006@gmail.com>
2025-05-18 16:12:14 +0700

    Update data.ts

--- app/data.ts
+++ app/data.ts
@@ -29,14 +29,14 @@ type SocialLink = {
 
 export const EMAIL = ''
 
-export const WEB_TITLE = 'Name - Personal Website'
+export const WEB_TITLE = 'Vinh - Personal Website'
 export const WEB_DESCRIPTION =
   'A personal website showcasing my work and projects.'
-export const FOOTER_COPYRIGHT = '© 2025 Nguyen Van B.'
+export const FOOTER_COPYRIGHT = '© 2025 Nguyen Tan Vinh.'
 export const FOOTER_LINK = 'https://github.com/username/'
 
-export const NAME = 'Nguyen Van B'
-export const JOB_TITLE = 'NASA Frontend Intern'
+export const NAME = 'Nguyen Tan Vinh'
+export const JOB_TITLE = 'Sinh vien'
 export const DESCRIPTION =
   'I am a passionate designer and developer with a focus on creating beautiful and functional user interfaces. I have experience in design engineering, front-end development, and product design. I love to explore the intersection of design and technology, and I am always looking for new challenges to tackle.'
 
